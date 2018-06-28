21908a653ff44fa727614dea2015721d381ed940
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-28 17:09:05 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist 
+  @@all = []
+  @@songs = []
+  attr_accessor :name 
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def new_song(song_name,genre)
+  song = Song.new(song_name,genre)
+  song.artist = self
+  @@songs << song
+end
+
+def songs
+  @@songs.select do |x|
+    x.artist == self
+  end
+
+def genres
+  songs.map do |x|
+    song.genre
+  end
